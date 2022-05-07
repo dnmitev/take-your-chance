b623f6a7fb1182103d9569f118b64f45754d90fd
Dimitar Mitev <dnmitev@gmail.com>
2022-05-07 18:54:03 +0300

    added player's logic

---
+++ ConsoleRoyale/ConsoleRoyale/Player/IPlayer.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleRoyale
+{
+    /// <summary>
+    /// Acts a contract of what a player should be able to do. The simplest here is to add money and provide info
+    /// about the current balance
+    /// </summary>
+    public interface IPlayer
+    {
+        /// <summary>
+        /// Provides info about the current balance for a given player
+        /// </summary>
+        /// <returns>The amount of money available in the player's waller</returns>
+        decimal GetBalance();
+
+        /// <summary>
+        /// Deposit(add) money to player's waller
+        /// </summary>
+        /// <param name="amount">An amount that should be added to the player's wallet.</param>
+        /// <returns>The new amount available after the addition.</returns>
+        decimal DepositToWallet(decimal amount);
+    }
+}
---
+++ ConsoleRoyale/ConsoleRoyale/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleRoyale
+{
+    public class Player : IPlayer
+    {
+        private decimal _balance;
+
+        public Player()
+        {
+            _balance = 0;
+        }
+
+        public decimal Balance { get => _balance; }
+
+        public decimal GetBalance() => _balance;
+
+        /// <inheritdoc />
+        /// <exception cref="ArgumentException">When amount is below 0 which is insufficient,
+        /// then a an exception is thrown to prevent faulty behaviour</exception>
+        public decimal DepositToWallet(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException($"The deposited amount cannot be below 0. Given amount is {amount}");
+            }
+
+            _balance += amount;
+            return _balance;
+        }
+    }
+}
