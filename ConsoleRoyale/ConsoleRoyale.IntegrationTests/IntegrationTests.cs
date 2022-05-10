2e1f02b6f170bc67a4e6cf0149f52d49bc2ef4ae
Dimitar Mitev <dnmitev@gmail.com>
2022-05-10 15:36:03 +0300

    added more tests

--- ConsoleRoyale/ConsoleRoyale.IntegrationTests/IntegrationTests.cs
+++ ConsoleRoyale/ConsoleRoyale.IntegrationTests/IntegrationTests.cs
@@ -10,7 +10,7 @@ namespace ConsoleRoyale.IntegrationTests
     public class IntegrationTests
     {
         private const decimal DEFAULT_BET = 5;
-        private const int DEFAULT_ITTERATIONS = int.MaxValue;
+        private const int DEFAULT_ITERATIONS = int.MaxValue;
 
         [TestMethod]
         public void Playing_Infinite_Games_Should_Throw_An_Exception_Due_To_Insuffucient_Amount_In_Wallet()
@@ -21,18 +21,53 @@ namespace ConsoleRoyale.IntegrationTests
             var player = new Player(100000);
             var rng = new RNGProvider();
             var game = new Game(player, rng);
-            var cmdProcessor = new SimpleCommandProcessor();
-            var retriever = new ConsoleRetriever();
-            var output = new ConsoleOutput();
-            var consoleGameEngine = new ConsoleGameEngine(game, cmdProcessor, retriever, output);
 
             Assert.ThrowsException<ArgumentException>(() =>
             {
-                for (int i = 0; i < DEFAULT_ITTERATIONS; i++)
+                for (int i = 0; i < DEFAULT_ITERATIONS; i++)
                 {
                     var outcome = game.Play(DEFAULT_BET);
                 }
             });
+        }
+
+        [TestMethod]
+        public void Playing_Infinite_Games_Should_Get_Expected_Distribution_Of_Wins_And_Loses()
+        {
+            var initialBalance = (decimal)Math.Pow(10, 8);
+            var player = new Player(initialBalance);
+            var rng = new RNGProvider();
+            var game = new Game(player, rng);
+
+            var x2Count = 0;
+            var x10Count = 0;
+            var lose = 0;
+
+            var iterations = (int)Math.Pow(10, 6);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                var outcome = game.Play(1);
+                if (outcome == 0)
+                {
+                    lose++;
+                }
+                else if (outcome <= 2)
+                {
+                    x2Count++;
+                }
+                else
+                {
+                    x10Count++;
+                }
+            }
+
+
+            Assert.IsTrue(x10Count * 100 / iterations <= 10);
+            Assert.IsTrue(x2Count * 100 / iterations <= 40);
+            Assert.IsTrue(lose * 100 / iterations <= 50);
+
+            Assert.IsTrue(game.Player.GetBalance() < initialBalance);
         }
     }
 }
